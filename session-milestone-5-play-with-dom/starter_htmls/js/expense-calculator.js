ef4c65da805364a93a4e7b0cc6b65569d95f8827
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-18 12:55:00 +0600

    done

---
+++ session-milestone-5-play-with-dom/starter_htmls/js/expense-calculator.js
@@ -0,0 +1,23 @@
+const insertButton = document.getElementById("expense-insert-btn");
+const expenseField = document.getElementById("expense-field");
+const amountField = document.getElementById("amount-field");
+
+const rowContainer = document.getElementById("row-container");
+
+const expenseSpan = document.getElementById("expense-amount");
+
+insertButton.addEventListener("click", function () {
+  const expense = expenseField.value;
+  const amount = Number(amountField.value);
+
+  const row = `<div class="flex justify-between" data-amount="${amount}">
+        <p>${expense}</p>
+        <p>${amount}</p>
+      </div>`;
+
+  let expenseAmount = Number(expenseSpan.innerText);
+  expenseAmount += amount;
+  expenseSpan.innerText = expenseAmount;
+
+  rowContainer.innerHTML += row;
+});
